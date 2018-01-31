2ef2252557b5b2a1a03a1dc239b24c554354f97f
pierreozoux <pierre@ozoux.net>
2018-01-31 11:44:01 +0100

    Remove the old certs

--- clean-certs.sh
+++ clean-certs.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -eux
-grep -wvFf <(ls /data/domains/) <(ls certs/*/*.csr | grep -vf <(echo -e "node-exporter\ncadvisor") | cut -d"/" -f2) | xargs -i% ls /system/haproxy/certs/%
+grep -wvFf <(ls /data/domains/) <(ls certs/*/*.csr | grep -vf <(echo -e "node-exporter\ncadvisor") | cut -d"/" -f2) | xargs -i% rm -rf /system/haproxy/certs/%
